598e7a3343bb29e038fcbd4b6d695b8c4c3ff53c
kyw910305 <62334497+kyw910305@users.noreply.github.com>
2021-02-01 15:25:50 +0900

    Add files via upload

---
+++ Opentutorials/Java_opentutorials/ProjectMethod/WhyMethod.java
@@ -0,0 +1,24 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WhyMethod {
+	
+	public static void main(String[] args) throws IOException {
+
+					 //인자, argument
+		System.out.println(twoTimes("a,", "-"));
+		FileWriter fw = new FileWriter("out.text");
+		fw.write(twoTimes("a", "*"));
+		fw.close();
+//		Email.send("Dovenwolf.g.com", "two times a", twoTimes("a", "&"));
+		
+	}
+	public static String twoTimes(String text, String delimiter) {
+		String out = "";
+		out = out + delimiter + "\n";
+		out = out + text  + "\n";
+		out = out + text + "\n";
+		return out;
+	
+	}
+}
