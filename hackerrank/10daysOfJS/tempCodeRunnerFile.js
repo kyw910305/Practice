1c2bc249e01d293126bc1aa902a968849bb5a1e2
kyw910305 <kyw910305@gmail.com>
2021-03-14 15:22:07 +0900

    Day 4 completed

--- hackerrank/10daysOfJS/tempCodeRunnerFile.js
+++ hackerrank/10daysOfJS/tempCodeRunnerFile.js
@@ -1,2 +1,13 @@
-
-  if (a == 0) {
+function getCount(objects) {
+  let o = 0;
+  console.log(objects.length);
+  for (let p in objects) {
+    console.log(objects[p].x, objects[p].y);
+  }
+  for (var i = 0; i < objects.length; i++) {
+    if (objects[i].x == objects[i].y) {
+      return o++;
+    }
+  }
+  console.log(o);
+}
