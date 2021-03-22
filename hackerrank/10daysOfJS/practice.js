1b8b13c0502165f568ff68c45abad904bdae234b
kyw910305 <kyw910305@gmail.com>
2021-03-23 00:53:50 +0900

    210323

--- hackerrank/10daysOfJS/practice.js
+++ hackerrank/10daysOfJS/practice.js
@@ -1,18 +1,14 @@
-/*
- * Modify and return the array so that all even elements are doubled and all odd elements are tripled.
- * 
- * Parameter(s):
- * nums: An array of numbers.
- */
-function modifyArray(nums) {
-    nums = nums.map(n => n % 2 == 0 ? n * 2 : n * 3);
-    return nums;
+function getMaxLessThanK(n, k) {
+  let bitWise = [];
+  let arrIndex = 0;
+  for (let i = 1; i < n; i++) {
+    for (let j = 2; j <= n; j++) {
+      bitWise[arrIndex] = i & j;
+      arrIndex++;
+    }
+    bitWise = bitWise.filter(x => x < k);
+  }
+  return Math.max(...bitWise);
 }
 
-
-function main() {
-    const n = 5;
-    // const a = (readLine()).split(' ').map(Number);
-    // console.log(modifyArray(a).toString().split(',').join(' '));
-    modifyArray(n);
-}
+  console.log(getMaxLessThanK(5, 2));
