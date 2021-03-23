7fda10ef73e1ba42287803f3df39de8538f20c8c
kyw910305 <kyw910305@gmail.com>
2021-03-23 17:32:37 +0900

    Update

--- hackerrank/10daysOfJS/practice.js
+++ hackerrank/10daysOfJS/practice.js
@@ -1,34 +1,21 @@
 'use strict';
-// The days of the week are: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
-function getDayName(dateString) {
-  let dayName;
-  // Write your code here
-  let date = new Date(dateString);
-  switch (date.getDay()) {
-    case 0 :
-      dayName = 'Sunday';
-    break;
-    case 1 :
-      dayName = 'Monday';
-    break;
-    case 2 :
-      dayName = 'Tuesday';
-    break;
-    case 3 :
-      dayName = 'Wednesday';
-    break;
-    case 4 :
-      dayName = 'Thursday';
-    break;
-    case 5 :
-      dayName = 'Friday';
-    break;
-    case 6 :
-      dayName = 'Saturday';
-    break;
-  }
-  return dayName;
+function regexVar() {
+  /*
+   * Declare a RegExp object variable named 're'
+   * It must match a string that starts and ends with the same vowel (i.e., {a, e, i, o, u})
+   */
+  
+  
+  /*
+   * Do not remove the return statement
+   */
+  return re;
 }
-const d = 2;
-const date = '11/10/2010';
-console.log(getDayName(date));
+
+
+function main() {
+  const re = regexVar();
+  const s = readLine();
+  
+  console.log(re.test(s));
+}
